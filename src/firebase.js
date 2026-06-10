cc9572ea2fb2f2495c9be661b4799daabf3593a4
Antonio Labinjan <alabinjan6@gmail.com>
2026-06-10 17:31:16 +0200

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpf7gkhVtZYc3kqEzoqUYrd0dZ-7qJ_M4",
-  authDomain: "demos-sample.firebaseapp.com",
-  projectId: "demos-sample",
-  storageBucket: "demos-sample.firebasestorage.app",
-  messagingSenderId: "1021768695491",
-  appId: "1:1021768695491:web:ec5eaad9727d90c93b6c76",
-  measurementId: "G-M50CPHPD1S"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 }
 
 // Firebase inicijalizacija
